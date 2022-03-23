bda260bab5b2d2b09053d7bef6b0f4c0c75f14a5
lauren <lcc33@duke.edu>
2022-03-22 20:45:16 -0400

    molecules now don't cross through eachother. Order of magnitude correct for k_on

--- molecules.py
+++ molecules.py
@@ -17,21 +17,26 @@ def get_R(theta, phi, eta):
   return np.matmul(np.matmul(R_eta, R_phi), R_theta)
 
 class Molecule:
-  def __init__(self, location):
+  def __init__(self, location, radius):
     self.location = location
     self.binding_partner = 0
+    self.radius = radius
     self.locations = [self.location.copy()]
     self.R = get_R(np.random.uniform()*2*np.pi, np.random.uniform()*np.pi, np.random.uniform()*2*np.pi)
-    self.dock_offset = np.matmul(np.array([0, 0, 1]), self.R) 
+    self.dock_offset = np.matmul(np.array([0, 0, radius]), self.R) 
     self.dock_locations = [self.dock_offset + self.location]
   
   def move(self):
-    D = 136e6 # nm2/s
+    D = 1.36e-6 / (100**2) * (10**10)**2# angstrom2/s
     dt = 10*1e-9 # s
     S = math.sqrt(2*D*dt)
     self.location[0] += S*np.random.normal()
     self.location[1] += S*np.random.normal()
     self.location[2] += S*np.random.normal()
+    if np.any((self.location > 160)|(self.location < 0)):
+      #self.location = np.random.randint(0, 160, 3).astype(float)
+      self.location = np.array([50, 50, 50], float)
+      self.dock_location = self.dock_offset + self.location
     self.locations.append(self.location.copy())
 
   def rotate(self):
@@ -53,10 +58,21 @@ class Molecule:
     self.id = id
 
   def plot(self, ax, t_step):
+    limits = 100
+    ax.set_xlim3d(-limits, limits)
+    ax.set_ylim3d(-limits, limits)
+    ax.set_zlim3d(-limits, limits)
     x = np.array([self.locations[t_step][0], self.dock_locations[t_step][0]])
     y = np.array([self.locations[t_step][1], self.dock_locations[t_step][1]])
     z = np.array([self.locations[t_step][2], self.dock_locations[t_step][2]])
     ax.plot3D(x, y, z)
+
+    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    x = np.cos(u)*np.sin(v) * self.radius
+    y = np.sin(u)*np.sin(v) * self.radius
+    z = np.cos(v) * self.radius
+    ax.plot_surface(x + self.locations[t_step][0], y + self.locations[t_step][1], z + self.locations[t_step][2], color='r', alpha=0.5)
+
 
 
 class Ligand(Molecule):
@@ -69,8 +85,13 @@ class Substrate(Molecule):
   
   def calculate_ligand_distances(self, ligands):
     for ligand in ligands:
-      distance = np.linalg.norm(self.location - ligand.dock_location)
-      if distance < 0.1:
+      distance = np.linalg.norm(self.dock_location - ligand.dock_location)
+      if distance < 2:
         self.binding_partner = ligand.id
         ligand.binding_partner = self.id
+      elif distance > 200:
+        self.binding_partner = -1
+        #ligand.location = np.array([50, 50, 50], float)
+        #self.location = np.array([50, 50, 50+42], float)
+
         
--- reaction.py
+++ reaction.py
@@ -4,26 +4,29 @@ import numpy as np
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 import molecules as mol
+import os
 
 class Reaction:
   def __init__(self, molecules):
     self.molecules = molecules
     id = 1
     self.end_reaction = False
+    self.binding = False
     for molecule in self.molecules:
       molecule.set_id(id)
       id += 1
     self.t_step = 0
     self.time = 0
     self.progress_reaction()
-    self.show_animation()
+    #self.show_animation()
 
   def progress_reaction(self):
     while self.end_reaction == False:
+   # while self.end_reaction == False and self.time < 0.000154734999999959:
       self.step_reaction()
       self.compute_distances()
       self.t_step += 1
-    print(self.t_step)
+      self.time += 10e-9
 
   def compute_distances(self):
     substrates = [molecule for molecule in self.molecules if type(molecule) == mol.Substrate]
@@ -32,30 +35,82 @@ class Reaction:
       substrate.calculate_ligand_distances(ligands)
       if substrate.binding_partner > 0:
         self.end_reaction = True
+        self.binding = True
+      elif substrate.binding_partner < 0:
+        self.end_reaction = True
+        self.binding = False
         break
+ 
 
   def show_animation(self):
     self.fig = plt.figure()
     self.ax = p3.Axes3D(self.fig)
-    limits = 20
-    if self.t_step < 500:
+    if self.t_step < 50000:
       for t_step in range(self.t_step):
         self.ax.clear()
-        self.ax.set_xlim3d(-limits, limits)
-        self.ax.set_ylim3d(-limits, limits)
-        self.ax.set_zlim3d(-limits, limits)
         for molecule in self.molecules:
           molecule.plot(self.ax, t_step)
-        plt.pause(0.01)
+        plt.pause(0.1)
     else:
       for molecule in self.molecules:
         molecule.plot(self.ax, self.t_step)
     plt.show()
-  
+    plt.savefig('output.png')
+
   def step_reaction(self):
     for molecule in self.molecules:
       molecule.move()
       molecule.rotate()
+    distance = np.linalg.norm(self.molecules[0].location - self.molecules[1].location)
+    if distance < 36:
+      for molecule in self.molecules:
+        molecule.location = molecule.locations[-2]
+        molecule.dock_location = molecule.dock_locations[-2]
+      self.time += -10e-9
+      self.t_step += -1
 
-molecules = [mol.Ligand(np.array([0, 10, 10])), mol.Substrate(np.array([0, 0, 10])), mol.Ligand(np.array([0, 0, 0])), mol.Substrate(np.array([10, 0, 0]))]
-Reaction(molecules)
+def scientific(input):
+  output = "{:e}".format(input)
+  return output
+
+radius = 18
+binding_count = 0
+total_count = 500
+rstart = 42
+total_time = np.zeros(total_count)
+binding = np.zeros(total_count)
+for ii in range(total_count):
+  molecules = [mol.Ligand(np.array([50, 50, 50], float), radius), mol.Substrate(np.array([50, 50, 50+rstart], float), radius)]
+  reaction = Reaction(molecules)
+  total_time[ii] = reaction.time
+  binding[ii] = reaction.binding
+  print(str(ii) + ' of ' + str(total_count))
+p = np.sum(binding)/len(binding)
+print(p)
+print(binding)
+print(total_time)
+plt.hist(total_time)
+plt.show()
+mean_time = np.mean(total_time)
+print(mean_time)
+volume = 4/3*np.pi*(100e-10)**3 #(160e-10)**3 * 1000
+moles = 1/6.022e23
+concentration = moles/volume
+#print(concentration)
+#k = total_count/(concentration*total_time)
+#print(scientific(k))
+#print(scientific(1/(mean_time*concentration)))
+D = 1.36e-6 / (100**2) 
+b = 42e-10
+c = 200e-10
+k_d_b = 4*np.pi*b*D
+k_d_c = 4*np.pi*c*D
+NA = 6.022e23
+k = k_d_b * p /(1-(1-p)*k_d_b/k_d_c) * 1000*NA
+print(scientific(k))
+
+#if p < 1:
+#  lam = np.log(1-p)/(-0.000154734999999959)
+#  t = 1/lam
+#  k = 1/(t*concentration)
+#  print(scientific(k))
